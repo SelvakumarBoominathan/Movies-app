ddf4c230efa0791a860aa7976b5f755016a90a57
Selvakumar Boominathan <selvans2k@gmail.com>
2025-10-28 10:10:46 +0530

    Add Search component and integrate into App

--- Movies_App/src/App.jsx
+++ Movies_App/src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Search from "./components/Search";
 
 const App = () => {
   return (
@@ -6,11 +7,13 @@ const App = () => {
       <div className="pattern">
         <div className="wrapper">
           <header>
+            <img src="./hero.png" alt="Hero Banner" />
             <h1>
               Find <span className="text-gradient">Movies</span> You'll Enjoy
               Without the Hustle
             </h1>
           </header>
+          <Search />
         </div>
       </div>
     </main>
