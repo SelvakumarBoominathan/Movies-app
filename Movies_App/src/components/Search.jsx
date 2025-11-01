d3c30a15b3f10efb2396d25ea2defe7ebf28d2d4
Selvakumar Boominathan <selvans2k@gmail.com>
2025-11-01 18:55:13 +0530

    Changes in props

--- Movies_App/src/components/Search.jsx
+++ Movies_App/src/components/Search.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
-const Search = () => {
-  return <div className="text-white text-3xl ">Seearch</div>;
+const Search = ({ searchTerm, setSearchTerm }) => {
+  return (
+    <div className="search">
+      <div className="image">
+        <img src="search.svg" alt="" />
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search for Movies..."
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Search;
